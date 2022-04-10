74301fbed9b9942eb3981f789eb4d1803cc89c8f
vinisp <aragao93.vinicius@gmail.com>
2022-04-10 20:32:31 -0300

    players de sucesso

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,21 @@ function App() {
         title="My First compontnt"
         children={<div>Outro Elemento HTML DENTRO DO BOX</div>}
       />
+      <iframe
+        title="FirstFrame"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/GNCd_ERZvZM"
+        frameBorder="0"
+        allowFullScreen
+      />
+      <iframe
+        title="Second Frame"
+        src="https://player.vimeo.com/video/689437748"
+        width="340"
+        height="360"
+        allow="autoplay; fullscreen; picture-in-picture"
+      />
     </div>
   );
 }
